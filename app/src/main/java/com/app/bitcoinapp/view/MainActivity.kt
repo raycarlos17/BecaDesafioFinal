c427a45852937d65652e8ba465967cae528df039
joseRobertoMoura <jose.roberto.moura@everis.com>
2021-04-13 13:40:36 -0300

    Criação do projeto

---
+++ app/src/main/java/com/app/bitcoinapp/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.app.bitcoinapp.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.app.bitcoinapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
