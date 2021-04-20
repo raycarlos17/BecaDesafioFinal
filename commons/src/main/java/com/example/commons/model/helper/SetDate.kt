a00a839134c718d6cf14c7315dc5253a2fe53032
Ray Carlos <raycarlos17@gmail.com>
2021-04-20 14:39:43 -0300

    Exclusão de variaveis e arquivos redundantes

--- commons/src/main/java/com/example/commons/model/helper/SetDate.kt
+++ commons/src/main/java/com/example/commons/model/helper/SetDate.kt
@@ -1,7 +1,5 @@
 package com.example.commons.model.helper
 
-import android.os.Build
-import androidx.annotation.RequiresApi
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
